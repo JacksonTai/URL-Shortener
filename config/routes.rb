3ef770813a3621c849669c6657a39707ce216ca1
Jackson Tai <jacksontai.dev@gmail.com>
2023-04-17 09:21:06 +0800

    fix: Routing for Urls
    - specify the http request that can be handled by the existing controller
    - remove redundant routing for Urls

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :urls
-  get '/urls/:id', to: 'urls#show'
+  resources :urls, only: [:create, :show]
   get '/:short_url', to: 'urls#redirect'
   get "*path", to: "application#render_404"
   
